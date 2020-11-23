e143983a39d3399ccea8adc5db6c38eeba769e68
CodexNine <dpvanderlaan@gmail.com>
2020-11-23 11:01:38 +0100

    Added two options: 1. Case statement option 2. ROM defined option (space intensive)

--- Python/readImage.py
+++ Python/readImage.py
@@ -3,12 +3,12 @@ from PIL import Image
 
 
 # Resize the image to fit 30x32 format RGB
-img = np.array(Image.open('grass_tile.png'))
-img = img[0:30, 0:32]
-Image.fromarray(img).save('resized.png')
+# img = np.array(Image.open('Images\\character_left.png'))
+# img = img[0:30, 0:32]
+# Image.fromarray(img).save('Images\\resized.png')
 
 # Open and manipulate the resized file
-png_file = Image.open('resized.png')
+png_file = Image.open('Images\\character_right.png')
 data = np.asarray(png_file)
 
 # Remove alpha channel data from data matrix
@@ -18,17 +18,28 @@ data = data[:,:,:3]
 data = data * (15/255)
 data = np.rint(data).astype(np.int32)
 
+# outF = open("C:\\Users\\David_vd_Laan\\Documents\\David\\TU_Delft\\20-21\\Q2\\EE2L11\\ModulesVHDL\\Grass_tile.txt", "w")
+print('case v_timer is')
 for i in range(0,30):
-    # print('when "', format(i, "05b"), '"=>')
-    # print("CASE h_timer IS")
+    print('when "', format(i, "05b"), '"=>')
+    print("CASE h_timer IS")
     for j in range(0,32):
-        # print('when "', format(j, "05b"), '"=>')
-        var1 = format(data[i][j][0], "04b")
-        var2 = format(data[i][j][1], "04b")
-        var3 = format(data[i][j][2], "04b")
-        sentence = '"'+var1+var2+var3+'",'
-        sentence.strip()
-        print(sentence)
+# CASE OPTION -- Takes less space than other options (preferred)
+        print('when "', format(j, "05b"), '"=>')
+        print('Red <="',format(data[i][j][0], "04b"),'";')
+        print('Green <="',format(data[i][j][1], "04b"),'";')
+        print('Blue <="',format(data[i][j][2], "04b"),'";')
+
+# ROM OPTION -- Takes more space, so not adviced.
+        # var1 = format(data[i][j][0], "04b")
+        # var2 = format(data[i][j][1], "04b")
+        # var3 = format(data[i][j][2], "04b")
+        # sentence = '"'+var1+var2+var3+'",'
+        # sentence.strip()
+        # outF.write(sentence)
+        # outF.write("\n")
+#
+# outF.close()
         # for m in range(0,3):
         #     if(m == 0):
         #         print('R_grass <= "',format(data[i][j][m], "04b"), '";')
